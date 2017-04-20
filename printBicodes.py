4d5a90f5cbd060a4bc4579af2a3444e7466f100f
Xueguang Lu <lu.xue@husky.neu.edu>
2017-04-20 00:46:19 -0400

    imporved printers ;

--- printBicodes.py
+++ printBicodes.py
@@ -5,22 +5,11 @@ def getPYON(path):
 def readBiCodes():
     return getPYON('savedBinaryStrings.pyon')
 
-def readClusters():
-    def reduceTuple(t):
-        return tuple(map(lambda x:x[0], t))
-    clusters = getPYON('savedClusters.pyon')
-    return map(reduceTuple,clusters.values())
-
-def clusterPrinter(c):
-    for index,item in enumerate(c):
-        print("{}. {}".format(index+1,item,len(item)))
-
 def bicodePrinter(b):
     for word,code in b.items():
         print('{} - {}\n'.format(word,code))
 
 def main():
-    clusterPrinter(readClusters())
     bicodePrinter(readBiCodes())
 
 if __name__ == '__main__':
